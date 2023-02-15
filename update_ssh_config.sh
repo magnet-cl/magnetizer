583b3339cb1d8658dea571f1190bdbc57f91008f
Gabriel Rojas <gabriel.rojas@magnet.cl>
2023-02-15 14:58:31 -0300

    fix: force macos users into their conf

--- update_ssh_config.sh
+++ update_ssh_config.sh
@@ -8,25 +8,16 @@ function print_green() {
 
 REPO='git@bitbucket.org/magnet-cl/keygen.git'
 SSH_CONFIG_FILE='ssh_config'
-
-ADD_MACOS_CONFIG_FLAG=''
 IDENTITY_FILE_PATH=''
 
 print_usage() {
 	printf "Usage:
-\t-m
-\t\tAdds the following configuration to ssh hosts:\n
-\t\t\tAddKeysToAgent yes
-\t\t\tUseKeychain yes
-\t\t\tIdentitiesOnly yes\n
-\t\tTo ensure macos uses keychain.\n
 \t-i FILE
 \t\tSpecify identity file for ssh hosts.\n
 "
 }
-while getopts 'mi:' flag; do
+while getopts 'i:' flag; do
 	case "${flag}" in
-	m) ADD_MACOS_CONFIG_FLAG='true' ;;
 	i) IDENTITY_FILE_PATH="${OPTARG}" ;;
 	*)
 		print_usage
@@ -40,14 +31,14 @@ git archive --remote=ssh://$REPO master $SSH_CONFIG_FILE | tar -x
 mkdir -p ~/.ssh/config.d
 mv -f $SSH_CONFIG_FILE ~/.ssh/config.d/magnet
 
-if [ ! -z "$ADD_MACOS_CONFIG_FLAG" ]; then
+if [[ "$OSTYPE" == "darwin"* ]]; then
 	print_green "Adding MacOS configuration to hosts"
-	perl -pi -e 's/(.*)user magnet/&\n\1AddKeysToAgent yes\n\1UseKeychain yes/g' ~/.ssh/config.d/magnet
+	perl -pi -e 's/(.*)user magnet/$&\n\1AddKeysToAgent yes\n\1UseKeychain yes/g' ~/.ssh/config.d/magnet
 fi
 
 if [ ! -z "$IDENTITY_FILE_PATH" ]; then
 	print_green "Adding identity file to host configuration"
-	perl -pi -e "s|(.*)user magnet|&\n\1IdentityFile $IDENTITY_FILE_PATH\n\1IdentitiesOnly yes|g" ~/.ssh/config.d/magnet
+	perl -pi -e "s|(.*)user magnet|\$&\n\1IdentityFile $IDENTITY_FILE_PATH\n\1IdentitiesOnly yes|g" ~/.ssh/config.d/magnet
 fi
 
 if grep -Fxq "Include config.d/*" ~/.ssh/config; then
@@ -56,5 +47,5 @@ if grep -Fxq "Include config.d/*" ~/.ssh/config; then
 else
 	# code if not found
 	print_green "Including config.d/* in ssh config"
-	perl -pi -e '1{h;s/.*/Include config.d\/*\n/;p;g;}' ~/.ssh/config
+	perl -pi -e 's/^/Include config.d\/*\n\n/' ~/.ssh/config
 fi
