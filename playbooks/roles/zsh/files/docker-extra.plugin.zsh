056324b190bf8a7437f70ec415b81bda8fb71287
Tomás Andrighetti <tomas.andrighetti@magnet.cl>
2023-06-13 16:36:26 -0400

    chore: remove dcupbd
    use built-in dcupdb instead

--- playbooks/roles/zsh/files/docker-extra.plugin.zsh
+++ playbooks/roles/zsh/files/docker-extra.plugin.zsh
@@ -1,5 +1,3 @@
 # See docker plugin Readme (~/.oh-my-zsh/plugins/docker/README.md)
 zstyle ':completion:*:*:docker:*' option-stacking yes
 zstyle ':completion:*:*:docker-*:*' option-stacking yes
-
-alias dcupbd="docker-compose up --build -d"
